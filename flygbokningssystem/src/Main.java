3b83ad79a3de5a8c4afac3245ee81f54df80ed42
MehdiByte <mehdi.muhebbi5@gmail.com>
2023-03-23 09:45:53 +0100

    la till en rad
    la till en rad

--- flygbokningssystem/src/Main.java
+++ flygbokningssystem/src/Main.java
@@ -1,5 +1,8 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello era fucking units");
+        System.out.println("Detta är Mehdi");
     }
 }
