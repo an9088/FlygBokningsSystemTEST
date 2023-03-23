242e30830456c4c661d06d2b18730d75336e7364
malmmattias <malmmattias1@outlook.com>
2023-03-23 10:04:31 +0100

    Radera allt

--- flygbokningssystem/src/Main.java
+++
@@ -1,8 +0,0 @@
-import java.sql.SQLOutput;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello era fucking units");
-        System.out.println("Detta är Mehdi");
-    }
-}
