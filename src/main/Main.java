ba6db839a827ccb1945bcf6418392521f782c366
malmmattias <malmmattias1@outlook.com>
2023-03-23 10:51:38 +0100

    Update Main.java
    Uppdaterade main metoden och la till ett nytt flygbolag

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,9 @@ package src.main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hej");
+
+        Airline airline = new Airline("WizzAir", "W123AN1324");
+        System.out.println("Added a new airline");
+        System.out.println(airline.toString());
     }
 }
