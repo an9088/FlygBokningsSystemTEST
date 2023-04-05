6c78e54f8ea5eb066f11218f63b7db8a9960a924
an9088 <midibasketi@gmail.com>
2023-04-05 19:59:28 +0200

    Initializing gui design
    Just initializing the third party library and making a new MainForm.form for my redesign of the gui

---
+++ src/view/MainFrameV2.java
@@ -0,0 +1,17 @@
+package src.view;
+
+
+import javax.swing.*;
+
+public class MainFrameV2 {
+    private JPanel panelMain;
+
+    public void setLookAndFeel() {
+        try {
+            UIManager.setLookAndFeel("de.javasoft.plaf.synthetica.SyntheticaStandardLookAndFeel");
+        } catch (Exception exception) {
+
+            exception.printStackTrace();
+        }
+    }
+}
