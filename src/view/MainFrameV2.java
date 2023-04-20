36194db8833f69155492a45481806cf3186f0642
an9088 <midibasketi@gmail.com>
2023-04-20 13:23:50 +0200

    added new stuff

--- src/view/MainFrameV2.java
+++ src/view/MainFrameV2.java
@@ -42,47 +42,6 @@ public class MainFrameV2 extends JFrame implements ActionListener {
 
 
 
-        // Create menu bar and icons
-        JMenuBar menuBar = new JMenuBar();
-
-        // Create menu items for file menu
-        fileMenu = new JMenu("File");
-         openItem = new JMenuItem("Open");
-         closeItem = new JMenuItem("Close");
-         exitItem = new JMenuItem("Exit");
-        fileMenu.add(openItem);
-        fileMenu.add(closeItem);
-        fileMenu.add(exitItem);
-
-        // Create menu items for options menu
-        options = new JMenu("Options");
-        toolTipsItem = new JCheckBoxMenuItem("Enable Tool Tips");
-        musicItem = new JCheckBoxMenuItem("Disable/Enable music");
-        options.add(toolTipsItem);
-        options.add(musicItem);
-
-        // Create menu items for help menu
-        help = new JMenu("Help");
-        homePage = new JMenuItem("Homepage");
-        about = new JMenuItem("About");
-        help.add(homePage);
-        help.add(about);
-
-        // Add menus to menu bar
-        menuBar.add(fileMenu);
-        menuBar.add(options);
-        menuBar.add(help);
-        setJMenuBar(menuBar);
-
-        // Add action listeners to menu items
-        openItem.addActionListener(this);
-        closeItem.addActionListener(this);
-        exitItem.addActionListener(this);
-        toolTipsItem.addActionListener(this);
-        musicItem.addActionListener(this);
-        homePage.addActionListener(this);
-        about.addActionListener(this);
-
         // Show window
         this.setVisible(true);
     }
@@ -91,26 +50,7 @@ public class MainFrameV2 extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e){
-        if(e.getSource()==exitItem){
-            System.exit(0);
-        }
 
-        if(e.getSource()==openItem){
-            System.out.println("You opened a file!");
-        }
-
-        if (e.getSource() == toolTipsItem) {
-            boolean isSelected = toolTipsItem.isSelected();
-
-
-        }
-
-        if (e.getSource() == musicItem) {
-
-            boolean isSelected = musicItem.isSelected();
-
-
-        }
     }
 
     public static void main (String[] args) throws ParseException {
