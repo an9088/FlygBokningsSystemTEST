79aa84f488987909560b444eb2b62747451ebd93
an9088 <midibasketi@gmail.com>
2023-04-23 16:18:13 +0200

    modification

--- src/view/Payment_Page.java
+++ src/view/Payment_Page.java
@@ -1,6 +1,9 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Payment_Page {
     private JPanel mainPanel;
@@ -18,4 +21,49 @@ public class Payment_Page {
     private JPanel paymentInfoPanel;
     private JPanel paymentInfoPanel2;
     private JButton confirmPayButton;
+
+
+    public Payment_Page(){
+
+        confirmPayButton.setText("Confirm & Pay");
+        confirmPayButton.setBackground(new Color(0, 123, 255));
+        confirmPayButton.setForeground(Color.WHITE);
+        confirmPayButton.setOpaque(true);
+        confirmPayButton.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+        confirmPayButton.setFocusPainted(false);
+        confirmPayButton.setBorderPainted(false);
+        confirmPayButton.setRolloverEnabled(false);
+        confirmPayButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        confirmPayButton.setBorder(BorderFactory.createLineBorder(new Color(0, 123, 255), 2, true));
+        confirmPayButton.setBorder(BorderFactory.createCompoundBorder(confirmPayButton.getBorder(), BorderFactory.createEmptyBorder(5, 10, 5, 10)));
+
+
+
+        confirmPayButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        // create a new instance of the Payment_Page form
+        Payment_Page paymentPage = new Payment_Page();
+
+        // configure the form
+        JFrame frame = new JFrame("Payment Page");
+        frame.setContentPane(paymentPage.mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.setLocationRelativeTo(null);
+
+        // display the form
+        frame.setVisible(true);
+    }
+
+    private void createUIComponents() {
+        confirmPayButton = new JButton();
+        // TODO: place custom component creation code here
+    }
 }
