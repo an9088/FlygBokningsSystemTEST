561f1c19c1bad6813cd37fe4800036064bc15e7b
malmmattias <malmmattias1@outlook.com>
2023-04-23 11:28:18 +0200

    lagt till menybar i programmet
    funktionalitet är ännu inte implementerad.

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,20 +1,14 @@
 package controller;
 
 import com.amadeus.exceptions.ResponseException;
-import com.formdev.flatlaf.FlatDarculaLaf;
-import com.formdev.flatlaf.FlatLightLaf;
-import com.formdev.flatlaf.intellijthemes.FlatArcDarkIJTheme;
-import com.formdev.flatlaf.intellijthemes.FlatDarkPurpleIJTheme;
 import com.formdev.flatlaf.intellijthemes.FlatOneDarkIJTheme;
-import org.xml.sax.SAXException;
-import model.AirportCode;
 import model.AmadeusAPI;
 import model.Booking;
+import org.xml.sax.SAXException;
 import view.Mainframe;
 
 import javax.swing.*;
 import javax.xml.parsers.ParserConfigurationException;
-import java.awt.*;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -26,7 +20,6 @@ public class Controller {
 
     private String message;
 
-    private AirportCode airportCode;
 
     private ArrayList<String> flightInfo;
 
@@ -41,7 +34,6 @@ public class Controller {
         }
 
         mainframe = new Mainframe(this);
-        this.airportCode = airportCode;
     }
 
 
