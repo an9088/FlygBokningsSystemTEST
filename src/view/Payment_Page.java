a42e8f474007d604ca9c1384a68f6b808f62891f
an9088 <midibasketi@gmail.com>
2023-04-23 16:02:54 +0200

    added a payment panel

---
+++ src/view/Payment_Page.java
@@ -0,0 +1,20 @@
+package view;
+
+import javax.swing.*;
+
+public class Payment_Page {
+    private JPanel mainPanel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JCheckBox checkBox1;
+    private JCheckBox checkBox2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JPanel contactPanel;
+    private JPanel paymentPanel;
+    private JPanel paymentMethodPanel;
+    private JPanel paymentInfoPanel;
+    private JPanel paymentInfoPanel2;
+}
