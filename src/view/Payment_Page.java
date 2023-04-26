6b559dd0e12f35fd5105dcfa98a714e3c4ffe374
an9088 <midibasketi@gmail.com>
2023-04-26 22:36:44 +0200

    minor changes to payment_page

--- src/view/Payment_Page.java
+++ src/view/Payment_Page.java
@@ -10,7 +10,6 @@ public class Payment_Page {
     private JTextField textField1;
     private JTextField textField2;
     private JCheckBox checkBox1;
-    private JCheckBox checkBox2;
     private JTextField textField3;
     private JTextField textField4;
     private JTextField textField5;
