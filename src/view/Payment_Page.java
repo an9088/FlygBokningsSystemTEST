b1ea45f4232f7e731af1396db3e14f130243c654
an9088 <midibasketi@gmail.com>
2023-04-28 17:11:54 +0200

    added new stuff

--- src/view/Payment_Page.java
+++ src/view/Payment_Page.java
@@ -1,34 +1,80 @@
 package view;
 
 import javax.swing.*;
+import javax.swing.text.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 public class Payment_Page {
     private JPanel mainPanel;
-    private JTextField textField1;
-    private JTextField textField2;
+    private JTextField nameField;
+    private JTextField emailField;
     private JCheckBox checkBox1;
-    private JTextField textField3;
-    private JTextField textField4;
+    private JTextField cardNumberField;
+    private JTextField nameCardField;
     private JPanel contactPanel;
     private JPanel paymentPanel;
     private JPanel paymentMethodPanel;
     private JPanel paymentInfoPanel;
     private JPanel paymentInfoPanel2;
     private JButton confirmPayButton;
-    private JTextField textField6;
-    private JTextField textField5;
+    private JTextField codeField;
+    private JTextField expiryDateField;
 
     public Payment_Page() {
         confirmPayButton.setText("Confirm & Pay");
         confirmPayButton.setBackground(new Color(0,123,255));
         confirmPayButton.setForeground(Color.WHITE);
-        int arc = 15;
-        confirmPayButton.setBorder(BorderFactory.create);
 
+
+
+
+
+        // Add suggestion text to text fields
+        addSuggestionText(codeField, "Security Code");
+        addSuggestionText(expiryDateField, "MM/YY");
+        addSuggestionText(cardNumberField, "Card Number");
+        addSuggestionText(nameCardField, "Name on Card");
+        addSuggestionText(nameField, "John Doe");
+        addSuggestionText(emailField, "johndoe123@gmail.com");
+
+        ((AbstractDocument) codeField.getDocument()).setDocumentFilter(new FixedLengthFilter(4));
+        ((AbstractDocument) cardNumberField.getDocument()).setDocumentFilter(new FixedLengthFilter(16));
+        ((AbstractDocument) expiryDateField.getDocument()).setDocumentFilter(new ExpiryDateFilter());
     }
+
+    private void addSuggestionText(JTextField field, String suggestion) {
+        // Set suggestion text color
+        Color gray = Color.GRAY;
+        field.setForeground(gray);
+
+        // Add focus listener to remove suggestion text when field gains focus
+        field.addFocusListener(new FocusListener() {
+            public void focusGained(FocusEvent e) {
+                if (field.getText().equals(suggestion)) {
+                    field.setText("");
+                    field.setForeground(Color.BLACK);
+                }
+            }
+
+            public void focusLost(FocusEvent e) {
+                if (field.getText().isEmpty()) {
+                    field.setText(suggestion);
+                    field.setForeground(gray);
+                }
+            }
+        });
+
+        // Set suggestion text initially if field is empty
+        if (field.getText().isEmpty()) {
+            field.setText(suggestion);
+        }
+    }
+
+
 
 
 
@@ -48,10 +94,68 @@ public class Payment_Page {
     }
 
 
-    private void createUIComponents() {
+    private class ExpiryDateFilter extends DocumentFilter {
+        @Override
+        public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr) throws BadLocationException {
+            String input = fb.getDocument().getText(0, fb.getDocument().getLength()) + string;
+            if (input.length() > 4) {
+                return;
+            }
 
+            super.insertString(fb, offset, string, attr);
 
+            if (input.length() == 2) {
+                fb.insertString(fb.getDocument().getLength(), "/", null);
+            }
+        }
 
+        @Override
+        public void replace(FilterBypass fb, int offset, int length, String text, AttributeSet attrs) throws BadLocationException {
+            String input = fb.getDocument().getText(0, fb.getDocument().getLength()) + text;
+            if (input.length() > 5) {
+                return;
+            }
 
+            super.replace(fb, offset, length, text, attrs);
+
+            if (input.length() == 2) {
+                fb.insertString(fb.getDocument().getLength(), "/", null);
+            }
+        }
     }
+
+    private class FixedLengthFilter extends DocumentFilter {
+        private int maxLength;
+
+        public FixedLengthFilter(int maxLength) {
+            super();
+            this.maxLength = maxLength;
+        }
+
+        @Override
+        public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr)
+                throws BadLocationException {
+            String input = fb.getDocument().getText(0, fb.getDocument().getLength()) + string;
+
+            if (input.length() <= maxLength ) {
+                return;
+            }
+        }
+
+        @Override
+        public void replace(FilterBypass fb, int offset, int length, String string, AttributeSet attrs)
+                throws BadLocationException {
+            String input = fb.getDocument().getText(0, fb.getDocument().getLength()) + string;
+
+            if (input.length() - length + string.length() <= maxLength) {
+                fb.replace(offset, length, string, attrs);
+            }
+        }
+    }
+
+
 }
+
+
+
+
