72453d6d2bec4a64f801952f3db57e8a54da8897
an9088 <midibasketi@gmail.com>
2023-04-29 00:06:33 +0200

    made a login  page class

---
+++ src/view/Login_Page.java
@@ -0,0 +1,11 @@
+package view;
+
+import javax.swing.*;
+
+public class Login_Page {
+    private JTextField textField1;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JButton button1;
+    private JPasswordField passwordField1;
+}
