5b3e51c8f8e508778d7dbb5a738b59564e812b14
malmmattias <malmmattias1@outlook.com>
2023-05-21 00:49:49 +0200

    lagt till bild i mail

--- src/model/automatedEmail.java
+++ src/model/automatedEmail.java
@@ -1,23 +1,14 @@
 package model;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import java.util.Properties;
-
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.util.*;
 
 public class automatedEmail {
 
     private String from, to, host, username, password;
-
     private String bookingInformation;
-
     private Properties properties;
-
 
     public automatedEmail(String to, String bookingInformation) {
         this.to = to;
@@ -27,8 +18,7 @@ public class automatedEmail {
 
     public void sendEmail(String to, String bookingInformation) {
         // Recipient's email ID needs to be mentioned.
-        //  to = "malmmattias1@outlook.com";
-
+        // to = "malmmattias1@outlook.com";
 
         // Sender's email ID needs to be mentioned
         from = "flightbuddyapplication@gmail.com";
@@ -37,9 +27,9 @@ public class automatedEmail {
         host = "smtp.gmail.com";
 
         // Set your Gmail account credentials
-        username = "flightbuddyapplication@gmail.com";
+        username = "FlightBuddyApplication@gmail.com";
         System.out.println(username);
-        password = "xuhxunycpqeawvbq";
+        password = "jefymmfzbusxshyr";
         System.out.println(password);
 
         // Get system properties
@@ -52,7 +42,6 @@ public class automatedEmail {
         properties.setProperty("mail.smtp.auth", "true");
 
         // Set username and password for authentication
-
         Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(username, password);
@@ -72,17 +61,35 @@ public class automatedEmail {
             // Set Subject: header field
             message.setSubject("This is your booking!");
 
-            // Now set the actual message
-            message.setText(bookingInformation);
+            // Create a multipart message
+            MimeMultipart multipart = new MimeMultipart();
 
-            // Send message
+            // Create the text part of the message
+            MimeBodyPart textPart = new MimeBodyPart();
+            textPart.setText(bookingInformation);
+
+            // Create the image part of the message
+            MimeBodyPart imagePart = new MimeBodyPart();
+            imagePart.attachFile("img/icons/FlightBuddy-4.png");
+
+            // Add the parts to the multipart message
+            multipart.addBodyPart(textPart);
+            multipart.addBodyPart(imagePart);
+
+            // Set the content of the message to the multipart
+            message.setContent(multipart);
+
+            // Send the message
             Transport.send(message);
             System.out.println("Sent message successfully....");
         } catch (MessagingException mex) {
             mex.printStackTrace();
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
 }
+
 
 
 
