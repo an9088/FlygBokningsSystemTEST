edefd6354ecb578250c0f3d41c9b1931fb2978e2
an9088 <midibasketi@gmail.com>
2023-05-20 11:56:52 +0200

    added a gui for the booking history

---
+++ src/view/Booking_History_Page.java
@@ -0,0 +1,10 @@
+package view;
+
+import javax.swing.*;
+
+public class Booking_History_Page {
+    private JPanel mainPanel;
+    private JList list1;
+    private JEditorPane editorPane1;
+    private JButton deleteBookingButton;
+}
